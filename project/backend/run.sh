191e0eee86f236f68c355f9b48efea8df422568b
EphorealTechnologies <tim22442244@gmail.com>
2024-11-24 02:51:26 +0530

    adding docker files, rm redundant lib files

--- project/backend/run.sh
+++ project/backend/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python -c 'import sys; print(sys.version_info[:])'
 pip install -r requirements.txt
-python3.11 -m uvicorn app.main:app --reload --port 8000
+python -m uvicorn app.main:app --reload --port 8000
